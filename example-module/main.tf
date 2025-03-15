c844c135e258d3e259093ab385afd251b587a59a
Craig Smith <infratex.admin@infratex.co.uk>
2025-03-15 22:09:45 +0000

    changed availability zone

--- example-module/main.tf
+++ example-module/main.tf
@@ -28,7 +28,7 @@ module "virtual_machine" {
   name                       = local.resource_names.virtual_machine_name
   sku_size                   = var.virtual_machine_sku
   location                   = var.location
-  zone                       = "1"
+  zone                       = "2"
   encryption_at_host_enabled = false
 
   source_image_reference = {
